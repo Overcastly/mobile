d293c0b668c85975d64de08a7970a678acf369d5
Josh Miller <joshtmiller04@gmail.com>
2024-12-01 22:15:55 -0500

    no more sending # in hastag array :(

--- lib/views/social/create_post.dart
+++ lib/views/social/create_post.dart
@@ -60,9 +60,9 @@ class _CreatePostState extends State<CreatePost> {
   void addHashtag() {
     final String hashtag = _hashtagController.text.trim();
 
-    if (hashtag.isNotEmpty && !hashtag.contains(' ') && !(hashtags.contains('#$hashtag'))) {
+    if (hashtag.isNotEmpty && !hashtag.contains(' ') && !(hashtags.contains('$hashtag'))) {
       setState(() {
-        hashtags.add('#$hashtag');
+        hashtags.add('$hashtag');
       });
       _hashtagController.clear();
     } else {
